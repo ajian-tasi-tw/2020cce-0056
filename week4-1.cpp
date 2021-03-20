79133b2985ceb0ac86ed4b1a323ee59e8bbb6dff
ajian-tsai <79628374+ajian-tsai@users.noreply.github.com>
2021-03-20 11:21:05 +0800

    Rename week4-1.cpp to week4/-1.cpp

--- week4-1.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-struct DATA{///宣告
-    float x,y,z;///定義裡面有什麼
-}
-int main()
-{
-
-}
---
+++ week4/-1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+struct DATA{///«Å§i
+    float x,y,z;///©w¸q¸Ì­±¦³¤°»ò
+}
+int main()
+{
+
+}
